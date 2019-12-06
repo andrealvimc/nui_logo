bb5da05dfc17e76f782384c801d12dd239530d06
MarmotaGit <40345645+MarmotaGit@users.noreply.github.com>
2019-12-06 00:42:29 -0300

    Optimized

--- main.lua
+++ main.lua
@@ -1,33 +1,11 @@
 -- Feito por Alvim
-local display = false
+-- Otimizado por MarmotaGit
+local display = true
 
-RegisterCommand("logoon", function()
-    Citizen.CreateThread(function()
-      TriggerEvent('nui:on', true)
-    end)
-end)
-  
-
-RegisterCommand("logooff", function()
-    Citizen.CreateThread(function()
-        TriggerEvent("nui:off", true)
-    end)
-end)
-
-
-
-RegisterNetEvent('nui:on')
-AddEventHandler('nui:on', function()
-    SendNUIMessage({
-    type = "ui",
-    display = true
-    })
-end)
-
-RegisterNetEvent('nui:off')
-AddEventHandler('nui:off', function()
+RegisterCommand("logo", function()
+    display = not display
     SendNUIMessage({
         type = "ui",
-        display = false
+        display = display
     })
 end)
